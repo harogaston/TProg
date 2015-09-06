488a0fe7c81ae751296e601aa34552d9109f3b9c
Marccio Silva <marccio.silva@fing.edu.uy>
2015-09-05 21:48:42 -0300

    Test ctrlUsuarios (pasando alrededor de un 90%)

--- ServidorCentral/src/com/tprog/logica/manejadores/ManejadorReservas.java
+++ ServidorCentral/src/com/tprog/logica/manejadores/ManejadorReservas.java
@@ -17,68 +17,73 @@ import java.util.Set;
 
 public class ManejadorReservas {
 
-	private Map<Integer, Reserva> reservas;
-	private static ManejadorReservas instance = null;
+    private Map<Integer, Reserva> reservas;
+    private static ManejadorReservas instance = null;
 
-	public static ManejadorReservas getInstance() {
-		if (instance == null) {
-			instance = new ManejadorReservas();
-		}
-		return instance;
-	}
-
-	private ManejadorReservas() {
-		reservas = new HashMap();
-	}
-
-	public Set<DTMinReserva> listarReservas() {
-		Set<DTMinReserva> set = new HashSet();
-		if (!reservas.isEmpty()) {
-			for (Reserva r : reservas.values()) {
-				DTMinReserva dtMin = r.crearDTMinReserva();
-				set.add(dtMin);
-			}
-		}
-		return set;
-	}
-
-	public DTReserva infoReserva(int idReserva) {
-		Reserva r = reservas.get(idReserva);
-		return r.crearDTReserva();
-	}
-
-	public boolean cambiarEstadoReserva(int idReserva, EstadoReserva nuevoEstado) {
-		Reserva r = reservas.get(idReserva);
-		return r.cambiarEstadoReserva(nuevoEstado);
-	}
-
-	public boolean eliminarReserva(int idReserva) {
-		Reserva r = reservas.get(idReserva);
-		EstadoReserva estado = r.getEstado();
-		if (estado == EstadoReserva.Registrada || estado == EstadoReserva.Cancelada) {
-                    r.getCliente().quitarReserva(idReserva);
-                    reservas.remove(idReserva);
-                    return true;
-		}
-		return false;
-	}
-
-	public void agregarReserva(Cliente cliente, DTReserva dtR, String nicknameP) throws Exception {
-		if (dtR != null) {
-                    Reserva nuevaReserva = new Reserva(cliente, dtR, nicknameP);
-                    reservas.put(nuevaReserva.getIdReserva(), nuevaReserva);
-                    cliente.agregarReserva(nuevaReserva);
-		} else {
-			System.out.println("El DTReserva que se paso hacia agregarReserva era nulo");
-		}
-
-	}
-
-	public EstadoReserva getEstadoReserva(int idReserva) {
-		Reserva r = reservas.get(idReserva);
-		return r.getEstado();
-	}
-        public Map<Integer,Reserva> getReservas(){
-                return reservas;
+    public static ManejadorReservas getInstance() {
+        if (instance == null) {
+            instance = new ManejadorReservas();
         }
+        return instance;
+    }
+
+    private ManejadorReservas() {
+        reservas = new HashMap();
+    }
+
+    public Set<DTMinReserva> listarReservas() {
+        Set<DTMinReserva> set = new HashSet();
+        if (!reservas.isEmpty()) {
+            for (Reserva r : reservas.values()) {
+                DTMinReserva dtMin = r.crearDTMinReserva();
+                set.add(dtMin);
+            }
+        }
+        return set;
+    }
+
+    public DTReserva infoReserva(int idReserva) {
+        Reserva r = reservas.get(idReserva);
+        if (r != null) {
+            return r.crearDTReserva();
+        } else {
+            return null;
+        }
+    }
+
+    public boolean cambiarEstadoReserva(int idReserva, EstadoReserva nuevoEstado) {
+        Reserva r = reservas.get(idReserva);
+        return r.cambiarEstadoReserva(nuevoEstado);
+    }
+
+    public boolean eliminarReserva(int idReserva) {
+        Reserva r = reservas.get(idReserva);
+        EstadoReserva estado = r.getEstado();
+        if (estado == EstadoReserva.Registrada || estado == EstadoReserva.Cancelada) {
+            r.getCliente().quitarReserva(idReserva);
+            reservas.remove(idReserva);
+            return true;
+        }
+        return false;
+    }
+
+    public void agregarReserva(Cliente cliente, DTReserva dtR, String nicknameP) throws Exception {
+        if (dtR != null) {
+            Reserva nuevaReserva = new Reserva(cliente, dtR, nicknameP);
+            reservas.put(nuevaReserva.getIdReserva(), nuevaReserva);
+            cliente.agregarReserva(nuevaReserva);
+        } else {
+            System.out.println("El DTReserva que se paso hacia agregarReserva era nulo");
+        }
+
+    }
+
+    public EstadoReserva getEstadoReserva(int idReserva) {
+        Reserva r = reservas.get(idReserva);
+        return r.getEstado();
+    }
+
+    public Map<Integer, Reserva> getReservas() {
+        return reservas;
+    }
 }

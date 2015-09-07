de321b62dbb2e49e257359e536d77b591f79d0cd
Marccio Silva <marccio.silva@fing.edu.uy>
2015-09-07 01:47:30 -0300

    Cambios de lógica de promoción aplicados a interfaz

--- ServidorCentral/src/com/tprog/logica/dt/DTMiniItem.java
+++ ServidorCentral/src/com/tprog/logica/dt/DTMiniItem.java
@@ -8,19 +8,27 @@ package com.tprog.logica.dt;
  * @author sofia
  */
 public class DTMiniItem {
+
     private DTMinServicio servicio;
     private int cantidad;
-    
-    public DTMiniItem(DTMinServicio servicio, int cantidad){
+
+    public DTMiniItem(DTMinServicio servicio, int cantidad) {
         this.servicio = servicio;
         this.cantidad = cantidad;
     }
-    
-    public DTMinServicio getDTMinServicio(){
+
+    public DTMinServicio getDTMinServicio() {
         return servicio;
     }
-    
-    public int getCantidad(){
+
+    public int getCantidad() {
         return cantidad;
     }
+
+    @Override
+    public String toString() {
+        return servicio.toString() + "Cantidad: "
+                + Integer.toString(cantidad) + "\n";
+    }
+
 }

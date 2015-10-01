8364501232ccb055fe791731c4b85ac57102d018
Gastón Haro <gaston.haro@fing.edu.uy>
2015-10-01 05:33:34 -0300

    Dummy servlet para testing.

---
+++ WebApp/src/tprog/web/VerServicio.java
@@ -0,0 +1,29 @@
+package tprog.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VerServicio extends HttpServlet {
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+		request.getRequestDispatcher("/pages/verServicio.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+}

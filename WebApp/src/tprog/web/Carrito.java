6a1519ba18a21646ba07abea1366f0a734a2bbb8
Marccio Silva <marccio.silva@fing.edu.uy>
2015-10-01 20:42:16 -0300

    Date picker de ver servicio y promocion conectado con servlet También se arregló un bug que ocurría cuando se generaba una línea de reserva de un servicio primero, y después de una promoción (en ctrlReservas::ingresarLineaReserva se hace un control medio raro y había que setear el dtS en null para poder ingresar una línea de una promoción)

--- WebApp/src/tprog/web/Carrito.java
+++ WebApp/src/tprog/web/Carrito.java
@@ -6,7 +6,12 @@
 package tprog.web;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -63,35 +68,47 @@ public class Carrito extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		processRequest(request, response);
-		//aca se reciben parametros y se modifica la reserva (agregando lineas)
-		HttpSession session = request.getSession(false);
-//		Map<LineaReserva, Integer> lineasReserva = (Map<LineaReserva, Integer>) session.getAttribute("lineasReserva");
-		//si no tengo una estructura en mi sesion para lineas de reserva, la inicializo
-//		if (lineasReserva == null) {
-//			lineasReserva = new HashMap<LineaReserva, Integer>();
-//		}
-		ICtrlReservas ctrlReservas = (ICtrlReservas) session.getAttribute("ctrlReservas"); //el controlador asociado a la sesion
-		ctrlReservas.seleccionarCliente((String) session.getAttribute("usuario_logueado")); //selecciono cliente
-		String idProveedor = request.getParameter("idProveedor");
-		if (request.getParameter("idServicio") != null) {
-			//en este caso llame al servlet desde un servicio
-			String idServicio = request.getParameter("idServicio");
-			DTMinServicio dt = new DTMinServicio(idProveedor, idServicio);
-			ctrlReservas.seleccionarServicio(dt);
+		try {
+			processRequest(request, response);
+			//aca se reciben parametros y se modifica la reserva (agregando lineas)
+			HttpSession session = request.getSession(false);
+			ICtrlReservas ctrlReservas = (ICtrlReservas) session.getAttribute("ctrlReservas"); //el controlador asociado a la sesion
+			ctrlReservas.seleccionarCliente((String) session.getAttribute("usuario_logueado")); //selecciono cliente
+			String idProveedor = request.getParameter("idProveedor");
+//			if (request.getParameter("idServicio") != null) { da lo mismo controlar así o con lo que está
+			if (request.getHeader("referer").contains("VerServicio")) {
+				//en este caso llame al servlet desde un servicio
+				String idServicio = request.getParameter("idServicio");
+				DTMinServicio dt = new DTMinServicio(idProveedor, idServicio);
+				ctrlReservas.seleccionarServicio(dt);
+			} else if (request.getHeader("referer").contains("VerPromocion")) {
+				//la llame desde una promocion
 
-		} else {
-			//la llame desde una promocion
-			String idPromocion = request.getParameter("idPromocion");
-			DTMinPromocion dt = new DTMinPromocion(idProveedor, idPromocion);
-			ctrlReservas.seleccionarPromocion(dt);
+				//esto es para evitar un problema de diseño del servidor central, que se fija si es null el dtServicio
+				//cuando en realidad tendría que preguntar qué se le está pidiendo
+				ctrlReservas.seleccionarServicio(null);
+				String idPromocion = request.getParameter("idPromocion");
+				DTMinPromocion dt = new DTMinPromocion(idProveedor, idPromocion);
+				ctrlReservas.seleccionarPromocion(dt);
+			}
+			//corregir la fecha de acuerdo a parámetros de entrada
+			int cantidad = Integer.parseInt(request.getParameter("cantidad"));
+
+			//obtengo strings para las fechas, en formato dd/mm/aaaa, hay que parsearlas
+			String fechaInicio = request.getParameter("inicio");
+			String fechaFin = request.getParameter("fin");
+
+			DateFormat sourceFormat = new SimpleDateFormat("dd/MM/yyyy");
+			Date dateInicio = sourceFormat.parse(fechaInicio);
+			Date dateFin = sourceFormat.parse(fechaFin);
+
+			ctrlReservas.ingresarLineaReserva(cantidad, dateInicio, dateFin);
+			session.setAttribute("cant_items", ((Integer) session.getAttribute("cant_items")) + 1);
+			session.setAttribute("reservaTemporal", ctrlReservas.mostrarReservaTemporal());
+			request.getRequestDispatcher("/pages/carrito.jsp").forward(request, response);
+		} catch (ParseException ex) {
+			Logger.getLogger(Carrito.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		//corregir la fecha de acuerdo a parámetros de entrada
-		int cantidad = Integer.parseInt(request.getParameter("cantidad"));
-		ctrlReservas.ingresarLineaReserva(cantidad, new Date(1992, 8, 10), new Date(1993, 8, 11));
-		session.setAttribute("cant_items", ((Integer) session.getAttribute("cant_items")) + 1);
-		session.setAttribute("reservaTemporal", ctrlReservas.mostrarReservaTemporal());
-		request.getRequestDispatcher("/pages/carrito.jsp").forward(request, response);
 	}
 
 	/**
--- WebApp/src/tprog/web/CerrarSesion.java
+++ WebApp/src/tprog/web/CerrarSesion.java
@@ -10,34 +10,35 @@ import javax.servlet.http.HttpSession;
 
 public class CerrarSesion extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        HttpSession objSesion = request.getSession();
-        EstadoSesion nuevoEstado = EstadoSesion.NO_LOGIN; // ahora da siempre login incorrecto dado que no estan cargados los datos
-        objSesion.setAttribute("estado_sesion", nuevoEstado);
-        objSesion.setAttribute("usuario_logueado", null);
-        //lo siguiente está comentado temporalmente porque "olvida" si los datos se cargaron
-//		objSesion.invalidate(); // creo q sirve para cuando se esta creando el carrito q se cierra sesion y borras todo
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/Home");
-        dispatcher.forward(request, response);
-    }
+		HttpSession objSesion = request.getSession();
+		EstadoSesion nuevoEstado = EstadoSesion.NO_LOGIN; // ahora da siempre login incorrecto dado que no estan cargados los datos
+		objSesion.setAttribute("ctrlReservas", null);
+		objSesion.setAttribute("reservaTemporal", null);
+		//objSesion.invalidate(); // creo q sirve para cuando se esta creando el carrito q se cierra sesion y borras todo
+		objSesion.setAttribute("usuario_logueado", null);
+		objSesion.setAttribute("estado_sesion", nuevoEstado);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/Home");
+		dispatcher.forward(request, response);
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
 
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 
 }

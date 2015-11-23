01a274e15482e97be6ae50af65210ccf8c035ae6
Marccio Silva <marccio.silva@fing.edu.uy>
2015-11-23 13:48:29 -0300

    Arreglo de import en persistencia

--- ServidorCentral/src/persistencia/FacturaF.java
+++ ServidorCentral/src/persistencia/FacturaF.java
@@ -18,6 +18,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import tprog.logica.dt.DTFacturaF;
+import tprog.logica.dt.DTPromocionF;
 import tprog.logica.dt.DTServicioF;
 
 /**
@@ -26,129 +27,133 @@ import tprog.logica.dt.DTServicioF;
  */
 @Entity
 public class FacturaF implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long identificador;
-    private int idReserva;
-    @Temporal(TemporalType.DATE)
-    private Date fecha;
-    private double monto;
-    private String nicknameCliente;
-    @ManyToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY)
-    @JoinTable(name="FACT_SERV", joinColumns=@JoinColumn(name="FACT_ID"), inverseJoinColumns=@JoinColumn(name="SERV_ID"))
-    private Collection<ServicioF> servicios;
-    @ManyToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY)
-    @JoinTable(name="FACT_PROM", joinColumns=@JoinColumn(name="FACT_ID"), inverseJoinColumns=@JoinColumn(name="PROM_ID"))
-    private Collection<PromocionF> promociones;
 
-    public int getIdReserva() {
-        return idReserva;
-    }
-/*
-    public Date getFecha() {
-        return fecha;
-    }
-*/
-    public double getMonto() {
-        return monto;
-    }
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long identificador;
+	private int idReserva;
+	@Temporal(TemporalType.DATE)
+	private Date fecha;
+	private double monto;
+	private String nicknameCliente;
+	@ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+	@JoinTable(name = "FACT_SERV", joinColumns = @JoinColumn(name = "FACT_ID"), inverseJoinColumns = @JoinColumn(name = "SERV_ID"))
+	private Collection<ServicioF> servicios;
+	@ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+	@JoinTable(name = "FACT_PROM", joinColumns = @JoinColumn(name = "FACT_ID"), inverseJoinColumns = @JoinColumn(name = "PROM_ID"))
+	private Collection<PromocionF> promociones;
 
-    public String getNicknameCliente() {
-        return nicknameCliente;
-    }
+	public int getIdReserva() {
+		return idReserva;
+	}
+	/*
+	 public Date getFecha() {
+	 return fecha;
+	 }
+	 */
 
-    public void setIdReserva(int idReserva) {
-        this.idReserva = idReserva;
-    }
-/*
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-*/
-    public void setMonto(double monto) {
-        this.monto = monto;
-    }
+	public double getMonto() {
+		return monto;
+	}
 
-    public void setNicknameCliente(String nicknameCliente) {
-        this.nicknameCliente = nicknameCliente;
-    }
+	public String getNicknameCliente() {
+		return nicknameCliente;
+	}
 
-    public void setServicios(Collection<ServicioF> servicios) {
-        this.servicios = servicios;
-    }
+	public void setIdReserva(int idReserva) {
+		this.idReserva = idReserva;
+	}
+	/*
+	 public void setFecha(Date fecha) {
+	 this.fecha = fecha;
+	 }
+	 */
 
-    public Collection<ServicioF> getServicios() {
-        return servicios;
-    }
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
 
-    public void setPromociones(Collection<PromocionF> promociones) {
-        this.promociones = promociones;
-    }
+	public void setNicknameCliente(String nicknameCliente) {
+		this.nicknameCliente = nicknameCliente;
+	}
 
-    public Collection<PromocionF> getPromociones() {
-        return promociones;
-    }
-    public Long getId() {
-        return identificador;
-    }
+	public void setServicios(Collection<ServicioF> servicios) {
+		this.servicios = servicios;
+	}
 
-    public void setId(Long identificador) {
-        this.identificador = identificador;
-    }
-    
-    public DTFacturaF crearDTFacturaF(){
-        //creo los dt de los servicios y promociones
-        Set<DTServicioF> dtServicios = new HashSet();
+	public Collection<ServicioF> getServicios() {
+		return servicios;
+	}
+
+	public void setPromociones(Collection<PromocionF> promociones) {
+		this.promociones = promociones;
+	}
+
+	public Collection<PromocionF> getPromociones() {
+		return promociones;
+	}
+
+	public Long getId() {
+		return identificador;
+	}
+
+	public void setId(Long identificador) {
+		this.identificador = identificador;
+	}
+
+	public DTFacturaF crearDTFacturaF() {
+		//creo los dt de los servicios y promociones
+		Set<DTServicioF> dtServicios = new HashSet();
 		Iterator<ServicioF> iterador = servicios.iterator();
 		while (iterador.hasNext()) {
 			ServicioF servicio = iterador.next();
 			DTServicioF temp = servicio.crearDTServicioF();
 			dtServicios.add(temp);
 		}
-        Set<DTPromocionF> dtPromociones = new HashSet();
+		Set<DTPromocionF> dtPromociones = new HashSet();
 		Iterator<PromocionF> iterador2 = promociones.iterator();
 		while (iterador2.hasNext()) {
-            PromocionF promocion = iterador2.next();
+			PromocionF promocion = iterador2.next();
 			DTPromocionF temp2 = promocion.crearDTPromocionF();
 			dtPromociones.add(temp2);
 		}
-        //ahora si creo el dt de la factura
-        DTFacturaF dtF = new DTFacturaF(idReserva, fecha, monto, nicknameCliente, dtServicios, dtPromociones);
-        return dtF;
-    }
+		//ahora si creo el dt de la factura
+		DTFacturaF dtF = new DTFacturaF(idReserva, fecha, monto, nicknameCliente, dtServicios, dtPromociones);
+		return dtF;
+	}
 
-    public Date getFecha() {
-        return fecha;
-    }
+	public Date getFecha() {
+		return fecha;
+	}
 
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (identificador != null ? identificador.hashCode() : 0);
-        return hash;
-    }
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the identificador fields are not set
-        if (!(object instanceof FacturaF)) {
-            return false;
-        }
-        FacturaF other = (FacturaF) object;
-        if ((this.identificador== null && other.identificador!= null) || (this.identificador!= null && !this.identificador.equals(other.identificador))) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (identificador != null ? identificador.hashCode() : 0);
+		return hash;
+	}
 
-    @Override
-    public String toString() {
-        return "persistencia.Factura[ id=" + identificador+ " ]";
-    }
-    
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the identificador fields are not set
+		if (!(object instanceof FacturaF)) {
+			return false;
+		}
+		FacturaF other = (FacturaF) object;
+		if ((this.identificador == null && other.identificador != null) || (this.identificador != null && !this.identificador.equals(other.identificador))) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "persistencia.Factura[ id=" + identificador + " ]";
+	}
+
 }
